c1187bab0584cc8b43c46afacbdbfb5a8d51931d
WalaceSilva03 <walace.jose00@gmail.com>
2025-04-02 23:36:49 -0300

    Restando Apenas 2.7, 2.5 e h

--- main.hs
+++ main.hs
@@ -47,35 +47,42 @@ ex4 = [x + 3 | x <- [2..39], x/=11, x/=20, x/=32, mod x 3 ==2]
 
 --e) [1.0,0.5,0.25,0.125,0.0625,0.03125]
 ex5 :: [Double]
-ex5 = [1 / (2^x) | x <- [0..5]]
+ex5 = [1 / 2^x | x <- [0..5]]
 
 --f) [1,10,19,28,37,46,55,64]
 ex6 :: [Int]
-ex6 = [ 1 + (x*9) | x <- [1..7]]
+ex6 = [ 1 + x*9 | x <- [1..7]]
 
 --g) [2,4,8,10,12,16,18,22,24,28,30]
 ex7 :: [Int]
-ex7 = [x + 2 | x <- [0..28], x/=4, x/=12, x/=18, x/=24, mod x 2 == 0]
+ex7 = [x + 2 | x <- [0..28], x/=4, x/=12, x/=18, x/=24, even x]
 
 --h) ['@','A','C','D','E','G','J','L'] -- PRECISA TERMINAR
-ex8 :: [Char]
+ex8 :: String
 ex8 = [x | x <- ['@'..'L'], x/='B', x/='F', x/='H', x/='I', x/='K']
 
 -- 2.2) Crie uma função que verifique se o tamanho de uma
 -- String é par ou não. Use Bool como retorno.
-
+ex9 :: String -> Bool
+ex9 x = even (length x)
 
 --2.3) Escreva uma função que receba um vetor de Strings e
 --retorne uma lista com todos os elementos em ordem reversa.
+ex10 :: [String] -> [String]
+ex10 xs = [reverse x | x <- xs]
 
 --2.4) Escreva uma função que receba um vetor de Strings e
 --retorne uma lista com o tamanho de cada String. As palavras de
 --tamanho par devem ser excluídas da resposta.
+ex11 :: [String] -> [Int]
+ex11 xs = [length x | x <- xs, odd (length x)]
 
 --2.5) Escreva a função head como composição de duas outras.
 
 --2.6) Faça uma função que receba uma String e retorne True
--- se esta for um palíndromo; caso contrário, False 
+-- se esta for um palíndromo; caso contrário, False
+ex13 :: String -> Bool
+ex13 x = x == reverse x 
 
 --2.7 Faça uma função que receba um inteiro e retorne uma
 --tupla, contendo: o dobro deste número na primeira coordenada, o
