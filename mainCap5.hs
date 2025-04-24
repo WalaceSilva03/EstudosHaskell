3bdc2702ec5bbeffba443d1f3529ee7df578e313
WalaceSilva03 <walace.jose00@gmail.com>
2025-04-23 23:53:40 -0300

    Todos os primeiros 5 exercícios feitos

--- mainCap5.hs
+++ mainCap5.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE UndecidableInstances #-}
-{-# OPTIONS_GHC -Wno-noncanonical-monoid-instances #-}
 import qualified Control.Monad.RWS as Data
 
 --5.1) Crie o tipo TipoProduto que possui os values constructors Escritorio , Informatica , Livro , Filme e Total. 
@@ -11,19 +9,17 @@ import qualified Control.Monad.RWS as Data
 --em valor . Explique como seria o exercício sem o uso de monoides. Qual(is) seria(m) a(s) diferença(s)?
 
 data TipoProduto = Escritorio | Informatica | Livro | Filme | Total deriving (Show, Eq)
-data Produto = Produto { valor :: Double, tp :: TipoProduto} | Nada deriving (Show, Eq)
-
-instance Monoid Produto where
-    mempty = Nada
+data Produto = Produto { valor :: Double, tp :: TipoProduto } | Nada deriving (Show, Eq)
 
 instance Semigroup Produto where
     (<>) Nada p = p
     (<>) p Nada = p
     (<>) (Produto v1 _) (Produto v2 _) = Produto (v1 + v2) Total
 
+instance Monoid Produto where
+    mempty = Nada
 
 -- 5.2) Crie uma função totalGeral que recebe uma lista de produtos e retorna o preço total deles usando o monoide anterior.
-
 totalGeral :: [Produto] -> Produto
 totalGeral = mconcat
 
@@ -33,12 +29,41 @@ totalGeral = mconcat
 --Quanto vale a expressão Min (-32) <> Min (-34) <> Min (-33) ?
 --Explique sua escolha para o mempty
 
-newtype Min = Min Int deriving (Show, Eq, Ord)
+data Min = Min Int deriving (Ord, Eq, Show)
 
 instance Semigroup Min where
     Min x <> Min y = Min (min x y)
 
 instance Monoid Min where
     mempty = Min maxBound
+
+--5.4) Crie uma função minAll que recebe um [Min] e retorna um Min contendo o menor valor.
+
+minAll :: [Min] -> Min
+minAll = mconcat
+
+--5.5) Crie o tipo Paridade com os values constructors Par e Impar . Crie o typeclass ParImpar que contém a função decide
+-- :: a -> Paridade e possui as instâncias: Para Int : noção de Par/Impar de Int
+-- Para [a] : uma lista de elementos qualquer é Par se o número de elementos o for.
+-- Bool : False como Par , True como Impar .
+
+data Paridade = Par | Impar deriving Show
+
+class ParImpar a where
+    decide :: a -> Paridade
+
+instance ParImpar Int where
+    decide x
+        | even x = Par
+        | otherwise = Impar
+
+instance ParImpar [a] where
+    decide x
+        | even (length x) = Par
+        | otherwise = Impar
+
+instance ParImpar Bool where
+    decide False = Par
+    decide True = Impar
 
 
